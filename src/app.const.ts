b6628bcc21505eacad4a7465830bb09f42782819
syu-y <syu@12580526@gmail.com>
2022-02-21 00:14:25 +0900

    YouTubeのChannelのベースURLを追加

--- src/app.const.ts
+++ src/app.const.ts
@@ -2,3 +2,4 @@ export const APP_NAME = 'Vtuber App';
 export const SITE_TITLE = 'Vtuber Web Site';
 export const HEADER_ICON_HEIGHT: number = 72;
 export const HEADER_ICON_WIDTH: number = HEADER_ICON_HEIGHT;
+export const URL_YOUTUBE_CHANNEL = 'https://www.youtube.com/channel/';
