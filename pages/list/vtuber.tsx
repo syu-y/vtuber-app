be1b9896f626f885933a23d0d06c80175ed00026
syu-y <syu@12580526@gmail.com>
2022-02-21 00:38:03 +0900

    一覧ページにAPIで取得した情報を表示（スタイリングや画面構成詳細未済）

--- pages/list/vtuber.tsx
+++ pages/list/vtuber.tsx
@@ -1,9 +1,24 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import type { InferGetStaticPropsType, NextPage } from 'next';
 import Link from 'next/link';
+import { getVtuberList } from '../../lib/api.vtuber-info';
 import PageLayout from '../../src/components/pageLayout';
+import Image from 'next/image';
+import { URL_YOUTUBE_CHANNEL } from '../../src/app.const';
 
-const VtuberListPage: NextPage = () => {
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export const getStaticProps = async () => {
+  const vtuberList = await getVtuberList();
+  return {
+    props: {
+      vtuberList
+    },
+    /** キャッシュ時間（ISR） */
+    revalidate: 30,
+  };
+}
+
+const VtuberListPage: NextPage<Props> = ({ vtuberList }) => {
   return (
     <PageLayout>
       <h1 className="title">Vtuber List</h1>
@@ -12,6 +27,24 @@ const VtuberListPage: NextPage = () => {
           <a>Go Home Page</a>
         </Link>
       </h2>
+        <ul>
+          {
+            vtuberList.map((v) => {
+              return (
+                <li key={v.vtuberId}>
+                  {v.youtubeInfo.title}
+                  <br />
+                  <Image
+                    priority
+                    src={v.youtubeInfo.icon}
+                    height={50}
+                    width={50}
+                    alt={v.youtubeInfo.title}
+                  /><a href={ URL_YOUTUBE_CHANNEL + v.youtubeInfo.channelId}>YouTube</a><br /><a href={v.twitterInfo.url}>Twitter</a>
+                </li>
+            );})
+          }
+        </ul>
     </PageLayout>
   );
 };
