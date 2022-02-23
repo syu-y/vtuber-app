e5354be9f64a779da08dc056286cda1d6465effd
syu-y <syu@12580526@gmail.com>
2022-02-23 17:36:02 +0900

    各種ユーティリティ作成

---
+++ src/utils/create-context.ts
@@ -0,0 +1,12 @@
+import { createContext, useContext } from 'react';
+
+/** コンテキストを生成 */
+export function createCtx<ContextType>() {
+  const ctx = createContext<ContextType | undefined>(undefined);
+  function useCtx() {
+    const c = useContext(ctx);
+    if (!c) throw new Error('useCtx must be inside a Provider with a value');
+    return c;
+  }
+  return [useCtx, ctx.Provider] as const;
+}
